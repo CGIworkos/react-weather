53276e9598bbcad9afb5506a05c0076c08bae469
archivizer.workos <archivizer.workos@gmail.com>
2021-02-09 18:02:51 +0200

    Add metric system for answer API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY ='9d214c2f2ee09765a9260d75105b7e60'
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`;
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(city) {
